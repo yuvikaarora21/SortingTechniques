80f4356b6fef41174202652093df2adfe309d635
yuvikaarora21 <76406536+yuvikaarora21@users.noreply.github.com>
2022-03-14 17:43:47 +0530

    Add files via upload

---
+++ selectionSort.java
@@ -0,0 +1,31 @@
+public class Main
+{
+    public static void printArray(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]+" ");
+        }
+    }
+	public static void main(String[] args) {
+      int arr[] = {7,8,3,1,2};
+      //selection sort
+      for(int i=0;i<arr.length-1;i++)
+      {
+          int smallest = i ;
+          
+          for(int j=i+1;j<arr.length;j++){
+          //swap
+         if(arr[smallest]>arr[j])
+         {
+             smallest=j;
+         }
+         
+      }
+      int temp=arr[smallest];
+      arr[smallest]=arr[i];
+      arr[i]=temp;
+	}
+	printArray(arr);
+}
+}
